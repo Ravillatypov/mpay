6e57f5e585236a504bd31b571ca60b4c00d5507f
Ravil Latypov <r.latypov@napoleonit.ru>
2019-07-06 10:34:57 +0300

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'acquiring-simple'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
